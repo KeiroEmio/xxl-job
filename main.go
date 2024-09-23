3c07e671d9758e22d548f15b3310bcf0106c65a2
KeiroEmio <2077405059@qq.com>
2024-09-23 15:47:46 +0800

    dev to origin server

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 
 func main() {
 	exec := xxl.NewExecutor(
-		xxl.ServerAddr("http://127.0.0.1:8080/xxl-job-admin"),
+		xxl.ServerAddr("http://165.22.51.161:8080/xxl-job-admin"),
 		xxl.AccessToken("default_token"), //请求令牌(默认为空)
-		xxl.ExecutorIp("127.0.0.1"),      //可自动获取
+		xxl.ExecutorIp("165.22.51.161"),  //可自动获取
 		xxl.ExecutorPort("9998"),         //默认9999（非必填）
 		xxl.RegistryKey("app-dapp"),      //执行器名称
 		xxl.SetLogger(&logger{}),         //自定义日志
